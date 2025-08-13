95f21a5b97b03c0f478a10f28ed818c9a1422b88
Mixer-one <99894586+Mixer-one@users.noreply.github.com>
2025-08-13 20:21:36 +0100

    PlayerCombat
    Player attacks and animations implemented

--- Assets/Temp Files/Ph Scripts/WeaponScript.cs
+++ Assets/Temp Files/Ph Scripts/WeaponScript.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class WeaponScript : MonoBehaviour
 {
     public float damage;
-
     private BoxCollider triggerBox;
     // Start is called before the first frame update
     void Start()
